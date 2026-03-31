931a259540bb6e14b983767607d34e0e65377ab2
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-03-31 12:21:58 +0530

    add a route for UpdateTask api

--- routes/routes.go
+++ routes/routes.go
@@ -15,10 +15,11 @@ func SetupRoutes(db *sql.DB) {
 	http.HandleFunc("GET /user/{userid}",handlers.GetUserById(db))
 	http.HandleFunc("GET /users/{userid}/tasks",handlers.GetTaskByUserId(db))
 	http.HandleFunc("GET /users/{userid}/tasks/{status}",handlers.GetTaskByStatus(db))
-	http.HandleFunc("PATCH /task/status",handlers.UpdateStatusOfTask(db))
+
+	http.HandleFunc("PATCH /users/{userid}/tasks/{taskid}",handlers.UpdateTask(db))
+
 	http.HandleFunc("GET /tasks/{userid}/{sort}",handlers.GetTasksBySorted(db))
 	http.HandleFunc("DELETE /users/{userid}/tasks/{taskid}",handlers.DeleteTask(db))
-	http.HandleFunc("PATCH /users/{userid}/tasks/{taskid}",handlers.RenameTask(db))
 
 	
 
