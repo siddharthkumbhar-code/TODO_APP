222830713bec1d6efd3afb598566cdb10c244db1
Soham Nagare <nagaresoham7@gmail.com>
2026-04-14 17:55:28 +0530

    add prometheus container  and add grafana container to visuliza the metrics of application using UI

--- Main.go
+++ Main.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	
 	"go-sqlite/database"
 	"go-sqlite/handlers"
+	"go-sqlite/metrics"
+	"go-sqlite/observability"
 	"go-sqlite/repository"
 	"go-sqlite/routes"
 	"go-sqlite/services"
-	"go-sqlite/observability"
 	"log"
 	"net/http"
 	"os"
 )
-
-
-// func InitTracer() func(context.Context) error {
-// 	ctx := context.Background()
-// 	exporter, err := otlptracegrpc.New(ctx,
-// 		otlptracegrpc.WithEndpoint("otel-collector:4317"),
-// 		otlptracegrpc.WithInsecure(),
-// 	)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	tp := sdktrace.NewTracerProvider(
-// 		sdktrace.WithBatcher(exporter),
-// 		sdktrace.WithResource(resource.NewWithAttributes(
-// 			semconv.SchemaURL,
-// 			semconv.ServiceName("todo-app"),
-// 		)),
-// 	)
-// 	otel.SetTracerProvider(tp)
-// 	return tp.Shutdown
-// }
 
 func main() {
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -56,11 +35,13 @@ func main() {
 	userservices := services.NewUserServices(repouser)
 	userhandler := handlers.NewUserHandler(userservices)
 
+	metrics.RegisterMetrics()
+
 	routes.SetupRoutes(taskhandler, userhandler, dbconn)
 
 	shutdown := observability.InitTracer()
 	defer shutdown()
-	
+
 	log.Println("server running on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
