407a1356fc152aa5b3aeb5b41713ef7c395943a3
siddharthkumbhar-code <siddharth.kumbhar@cctech.co.in>
2026-04-14 18:36:37 +0530

    logs observability done

--- Main.go
+++ Main.go
@@ -1,38 +1,42 @@
 package main
 
 import (
-	
+	"context"
 	"go-sqlite/database"
 	"go-sqlite/handlers"
 	"go-sqlite/repository"
 	"go-sqlite/routes"
 	"go-sqlite/services"
-	"go-sqlite/observability"
 	"log"
 	"net/http"
 	"os"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
 )
 
 
-// func InitTracer() func(context.Context) error {
-// 	ctx := context.Background()
-// 	exporter, err := otlptracegrpc.New(ctx,
-// 		otlptracegrpc.WithEndpoint("otel-collector:4317"),
-// 		otlptracegrpc.WithInsecure(),
-// 	)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	tp := sdktrace.NewTracerProvider(
-// 		sdktrace.WithBatcher(exporter),
-// 		sdktrace.WithResource(resource.NewWithAttributes(
-// 			semconv.SchemaURL,
-// 			semconv.ServiceName("todo-app"),
-// 		)),
-// 	)
-// 	otel.SetTracerProvider(tp)
-// 	return tp.Shutdown
-// }
+func InitTracer() func(context.Context) error {
+	ctx := context.Background()
+	exporter, err := otlptracehttp.New(ctx,
+		otlptracehttp.WithEndpoint("otel-connector:4317"),
+		otlptracehttp.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("todo-app"),
+		)),
+	)
+	otel.SetTracerProvider(tp)
+	return tp.Shutdown
+}
 
 func main() {
 	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -57,9 +61,8 @@ func main() {
 	userhandler := handlers.NewUserHandler(userservices)
 
 	routes.SetupRoutes(taskhandler, userhandler, dbconn)
-
-	shutdown := observability.InitTracer()
-	defer shutdown()
+	shutdown := InitTracer()
+	defer shutdown(context.Background())
 	
 	log.Println("server running on port 8080")
 	http.ListenAndServe(":8080", nil)
--- go.mod
+++ go.mod
@@ -6,8 +6,11 @@ require github.com/mattn/go-sqlite3 v1.14.34 // direct
 
 require (
 	go.opentelemetry.io/otel v1.43.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.43.0
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.19.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.43.0
+	go.opentelemetry.io/otel/log v0.19.0
 	go.opentelemetry.io/otel/sdk v1.43.0
+	go.opentelemetry.io/otel/sdk/log v0.19.0
 )
 
 require (
@@ -19,7 +22,6 @@ require (
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.28.0 // indirect
 	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.43.0 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.43.0 // indirect
 	go.opentelemetry.io/otel/metric v1.43.0 // indirect
 	go.opentelemetry.io/otel/trace v1.43.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.10.0 // indirect
